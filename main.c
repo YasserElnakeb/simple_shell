99755403eb574afc14d0c6be9f6115285ca258e9
Ayman-zain <aymnzyn85@gmail.com>
2023-09-23 04:14:11 +0300

    path

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+/**
+ * main - entry point
+ * @ac: number of args
+ * @av: array of args
+ * Return: 0;
+ */
+int main(int ac, char **argv){
+(void)ac;
+(void)argv;
+hsh();
+return (0);
+}
